b17a873da43c0beb62d474e4ae5e63635126cfba
Aidan De Angelis <angaidan@amazon.com>
2024-11-24 14:57:00 -0800

    wip: doing some cleaning

--- flake.nix
+++ flake.nix
@@ -2,57 +2,70 @@
   description = "A plugin for tmux that allows users to select actions from a customizable popup menu";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-    }:
-    let
-      inherit (nixpkgs) lib;
-      systems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-        "aarch64-linux"
-      ];
-      pkgsFor = lib.genAttrs systems (system: import nixpkgs { inherit system; });
-      forAllSystems = f: lib.genAttrs systems (system: f pkgsFor.${system});
-      version = self.sourceInfo.shortRev or self.sourceInfo.dirtyShortRev;
-    in
-    {
-      formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
+    inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      toplevel@{ withSystem, ... }:
+      {
+        imports = [
+          # To import a flake module
+          # 1. Add foo to inputs
+          # 2. Add foo as a parameter to the outputs function
+          # 3. Add here: foo.flakeModule
+        ];
 
-      packages = forAllSystems (pkgs: {
-        default = pkgs.callPackage ./nix { inherit version; };
-      });
+        debug = true; # This exposes declarations for nixd lsp
 
-      homeManagerModules.default = import ./nix/home-manager.nix self;
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+          "aarch64-darwin"
+          "x86_64-darwin"
+        ];
 
-      overlays.default = final: prev: {
-        tmuxPlugins = prev.tmuxPlugins // {
-          tmux-which-key = final.default;
+        perSystem =
+          {
+            config,
+            self',
+            inputs',
+            pkgs,
+            system,
+            ...
+          }:
+          {
+            packages.tmux-which-key =
+              with pkgs;
+              tmuxPlugins.mkTmuxPlugin {
+                pluginName = "tmux-which-key";
+                version = "dev";
+                src = lib.cleanSource ./.;
+                rtpFilePath = "plugin.sh.tmux";
+
+                propagatedBuildInputs = [
+                  check-jsonschema
+                  (python3.withPackages (ps: with ps; [ pyyaml ]))
+                ];
+
+                postInstall = # bash
+                  ''
+                    find $target -type f -name '*.sh' -exec chmod +x {} \;
+                    find $target -type f -name '*.tmux' -exec chmod +wx {} \;
+                  '';
+              };
+
+            packages.default = self'.packages.tmux-which-key;
+          };
+
+        flake = {
+          # The usual flake attributes can be defined here, including system-
+          # agnostic ones like nixosModule and system-enumerating ones, although
+          # those are more easily expressed in perSystem.
+
         };
-      };
-
-      apps = forAllSystems (
-        pkgs:
-        let
-          defaultConfig = import ./nix/generate-config.nix {
-            inherit lib pkgs;
-          };
-        in
-        {
-          generate-config = {
-            name = "generate-config";
-            type = "app";
-            program = "${pkgs.writeShellScriptBin "generate-config" ''
-              echo '${lib.generators.toPretty { } defaultConfig}';
-            ''}/bin/generate-config";
-          };
-        }
-      );
-    };
+      }
+    );
 }
